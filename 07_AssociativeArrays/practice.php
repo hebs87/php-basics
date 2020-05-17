74643450e01a048899e5bd2bb255d2a428564fb8
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-05-17 14:48:03 +0100

    Completed Associative Arrays practice exercise

--- 07_AssociativeArrays/practice.php
+++ 07_AssociativeArrays/practice.php
@@ -1,36 +1,43 @@
 <?php
 	
 	// Constants
-	
+	define('TITLE', 'Associative Arrays');
 	
 	// Custom Variables
-
+    $lesson_num = 6;
+    $this_year = date('Y');
+    $my_name = 'Sunny';
 	
 	// Moustache Associative Array
+    $moustache = array(
+        'name' => 'Handlebar',
+        'creep_factor' => 'High',
+        'days' => 14
+    );
 
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
 	<head>
-		<title>PHP <!-- PAGE TITLE --></title>
-		<link href="/assets/styles.css" rel="stylesheet">
+		<title>PHP <?php echo TITLE; ?></title>
+		<link href="../assets/styles.css" rel="stylesheet">
 	</head>
 	<body>
 		<div class="wrapper">
 			<a href="/" title="Back to directory" id="logo">
-				<img src="/assets/img/logo.png" alt="PHP">
+				<img src="../assets/img/logo.png" alt="PHP">
 			</a>
 			
-			<h1>Tutorial <!-- LESSON NUMBER -->: <small><!-- PAGE TITLE --></small></h1>
+			<h1>Tutorial <?php echo $lesson_num; ?>: <small><?php echo TITLE; ?></small></h1>
 			<hr>
 			
 			<h2>Your Example</h2>
 			
 			<div class="sandbox">
 			
-				<h2>The <!-- MOUSTACHE NAME --> Moustache!</h2>
-				<p>This moustache is quite the dirt squirrel! It boasts a creep factor of <strong><!-- CREEP FACTOR --></strong> and takes <strong><!-- GROWTH DAYS --> days</strong> to grow on average.</strong></p>
+				<h2>The <?php echo $moustache['name']; ?> Moustache!</h2>
+				<p>This moustache is quite the dirt squirrel! It boasts a creep factor of <strong><?php echo $moustache['creep_factor']; ?></strong> and takes <strong><?php echo $moustache['days']; ?> days</strong> to grow on average.</strong></p>
 				
 			</div><!-- end sandbox -->
 			
@@ -38,7 +45,7 @@
 			
 			<hr>
 			
-			<small>&copy;<!-- YEAR --> - <!-- YOUR NAME --></small>
+			<small>&copy;<?php echo $this_year; ?> - <?php echo $my_name; ?></small>
 		</div><!-- end wrapper -->
 		
 		<div class="copyright-info">
