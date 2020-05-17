5e5aa85290ce19898c809b471f64008a7a1eecc1
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-05-17 15:58:07 +0100

    Completed else statements challenge

--- 11_Else/practice.php
+++ 11_Else/practice.php
@@ -1,40 +1,50 @@
 <?php
-	
-	// Constants
 
-	
-	// Custom Variables
+    // Constants
+    define('TITLE', 'Arrays');
 
+    // Custom Variables
+    $lesson_num = 11;
+    $this_year = date('Y');
+    $my_name = 'Sunny';
+
+    $fav_fruit = 'mango';
 
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
 	<head>
-		<title>PHP <!-- TITLE --></title>
-		<link href="../assets/styles.css" rel="stylesheet">
+        <title>PHP <?php echo TITLE; ?></title>
+        <link href="../assets/styles.css" rel="stylesheet">
 	</head>
 	<body>
 		<div class="wrapper">
 			<a href="/" title="Back to directory" id="logo">
 				<img src="../assets/img/logo.png" alt="PHP">
 			</a>
-			
-			<h1>Tutorial <!-- LESSON NUMBER -->: <small><!-- TITLE --></small></h1>
-			<hr>
+
+            <h1>Tutorial <?php echo $lesson_num; ?>: <small><?php echo TITLE; ?></small></h1>
+            <hr>
 			
 			<h2>Your Example</h2>
 			
 			<div class="sandbox">
-				
+				<?php
+                    if ($fav_fruit == 'mango') {
+                        echo 'Yay, mangoes are the best';
+                    } else {
+                        echo 'So you don\'t like mangoes';
+                    };
+                ?>
 			</div><!-- end sandbox -->
 			
 			<a href="index.php" class="button">Back to the lecture</a>
 			
 			<hr>
-			
-			<small>&copy;<!-- YEAR --> - <!-- NAME --></small>
-		</div><!-- end wrapper -->
+
+            <small>&copy;<?php echo $this_year; ?> - <?php echo $my_name; ?></small>
+        </div><!-- end wrapper -->
 		
 		<div class="copyright-info">
 			<?php include('../assets/includes/copyright.php'); ?>
