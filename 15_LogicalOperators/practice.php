391b9d67df0b2f36b0a11fe49b0e6b2f0ec5344e
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-05-17 21:58:18 +0100

    Completed logical operators challenge

--- 15_LogicalOperators/practice.php
+++ 15_LogicalOperators/practice.php
@@ -1,68 +1,97 @@
 <?php
-	
-	// Constants
 
-	
-	// Custom Variables
+    // Constants
+    define("TITLE", "Logical Operators");
 
+    // Custom Variables
+    $myName = "Sunny";
+    $lessonNum = 15;
+    $this_year = date('Y');
+
+    $username = "hebs87";
+    $password = "qwerty";
+
+    $cartTotal = 19.99;
+    $couponCode = "DiscountPlease";
 
 ?>
 
 <!DOCTYPE html>
-<html>
-	<head>
-		<title>PHP <!-- TITLE --></title>
-		<link href="../assets/styles.css" rel="stylesheet">
-	</head>
-	<body>
-		<div class="wrapper">
-			<a href="/" title="Back to directory" id="logo">
-				<img src="../assets/img/logo.png" alt="PHP">
-			</a>
-			
-			<h1>Tutorial <!-- LESSON NUMBER -->: <small><!-- TITLE --></small></h1>
-			<hr>
-			
-			<h2>Your Example</h2>
-			
-			<div class="sandbox">
-				
-				<h3>And <code>and</code></h3>
-				<?php
-					// your code here
-				?>
-				
-				<h3>Or <code>or</code></h3>
-				<?php
-					// your code here
-				?>
-				
-				<h3>Not <code>!</code></h3>
-				<?php
-					// your code here
-				?>
-				
-				<h3>And <code>&amp;&amp;</code></h3>
-				<?php
-					// your code here
-				?>
-				
-				<h3>Or <code>||</code></h3>
-				<?php
-					// your code here
-				?>
-				
-			</div><!-- end sandbox -->
-			
-			<a href="index.php" class="button">Back to the lecture</a>
-			
-			<hr>
-			
-			<small>&copy;<!-- YEAR --> - <!-- NAME --></small>
-		</div><!-- end wrapper -->
-		
-		<div class="copyright-info">
-			<?php include('../assets/includes/copyright.php'); ?>
-		</div><!-- end copyright-info -->
-	</body>
+<html lang="en">
+<head>
+    <title>PHP <?php echo TITLE; ?></title>
+    <link href="../assets/styles.css" rel="stylesheet">
+</head>
+<body>
+<div class="wrapper">
+    <a href="/" title="Back to directory" id="logo">
+        <img src="../assets/img/logo.png" alt="PHP">
+    </a>
+
+    <h1>Tutorial <?php echo $lessonNum; ?>: <small><?php echo TITLE; ?></small></h1>
+    <hr>
+
+    <h2>Final Example</h2>
+    <p>Make sure to view the source code in your editor!</p>
+
+    <div class="sandbox">
+
+        <h3>And <code>and</code></h3>
+        <?php
+            if ($username == "hebs87" and $password == "qwerty") {
+                echo "<p>Login info is correct!</p>";
+            }
+        ?>
+
+        <h3>Or <code>or</code></h3>
+        <?php
+            if ($cartTotal > 15 or $couponCode == "DiscountPlease") {
+                echo "You get a discount!";
+            } else {
+                echo "You don't get a discount!";
+            }
+        ?>
+
+        <h3>Not <code>!</code></h3>
+        <?php
+            $ownDog = true;
+
+            if (!$ownDog) {
+                echo "You do not own a dog.";
+            } else {
+                echo "You own a dog.";
+            }
+        ?>
+
+        <h3>And <code>&amp;&amp;</code></h3>
+        <?php
+            if ($username == "johnnyboy" && $password == "qwerty") {
+                echo "<p>Login info is correct!</p>";
+            } else {
+                echo "<p>Wrong login info</p>";
+            }
+        ?>
+
+        <h3>Or <code>||</code></h3>
+        <?php
+            if ($cartTotal > 15 || $couponCode == "DiscountPlease") {
+                echo "You get a discount!";
+            } else {
+                echo "You don't get a discount!";
+            }
+        ?>
+
+    </div><!-- end sandbox -->
+
+    <a href="practice.php" class="button">Check out your example</a>
+
+    <hr>
+
+    <small>&copy;<?php echo $this_year; ?> - <?php echo $myName; ?></small>
+</div><!-- end wrapper -->
+
+<div class="copyright-info">
+    <?php include('../assets/includes/copyright.php'); ?>
+</div><!-- end copyright-info -->
+</body>
 </html>
