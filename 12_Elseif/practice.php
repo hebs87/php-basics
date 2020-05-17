1b5b8b4e95f2dd705c8be7315536f67f307e32b4
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-05-17 21:29:40 +0100

    Complete else if challenge

--- 12_Elseif/practice.php
+++ 12_Elseif/practice.php
@@ -1,40 +1,52 @@
 <?php
 	
 	// Constants
+    define("TITLE", "Else if");
 
-	
-	// Custom Variables
+    // Custom Variables
+    $this_year = date('Y');
+    $lesson_num = 12;
+    $my_name = "Brad Hussey";
 
+    $native_language = "Spanish";
 
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
 	<head>
-		<title>PHP <!-- TITLE --></title>
-		<link href="../assets/styles.css" rel="stylesheet">
+        <title>PHP <?php echo TITLE; ?></title>
+        <link href="../assets/styles.css" rel="stylesheet">
 	</head>
 	<body>
 		<div class="wrapper">
 			<a href="/" title="Back to directory" id="logo">
 				<img src="../assets/img/logo.png" alt="PHP">
 			</a>
-			
-			<h1>Tutorial <!-- LESSON NUMBER -->: <small><!-- TITLE --></small></h1>
-			<hr>
+
+            <h1>Tutorial <?php echo $lesson_num; ?>: <small><?php echo TITLE; ?></small></h1>
+            <hr>
 			
 			<h2>Your Example</h2>
 			
 			<div class="sandbox">
-				
-			</div><!-- end sandbox -->
+                <?php
+                    if ($native_language == "French") {
+                        echo "Bonjour! Vouz parlez Fran&ccedil;ais.";
+                    } else if ($native_language == "Spanish") {
+                        echo "&iexcl;Hola! Usted habla Espa&ntilde;ol.";
+                    } else {
+                        echo "Hello! You probably speak English.";
+                    }
+                ?>
+            </div><!-- end sandbox -->
 			
 			<a href="index.php" class="button">Back to the lecture</a>
 			
 			<hr>
-			
-			<small>&copy;<!-- YEAR --> - <!-- NAME --></small>
-		</div><!-- end wrapper -->
+
+            <small>&copy;<?php echo $this_year; ?> - <?php echo $my_name; ?></small>
+        </div><!-- end wrapper -->
 		
 		<div class="copyright-info">
 			<?php include('../assets/includes/copyright.php'); ?>
