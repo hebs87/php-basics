d9e04ce673122bd1e49c8f8293e9273ed6e27050
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-05-17 22:01:56 +0100

    Completed arithmetic operators challenge

--- 16_ArithmeticOperators/practice.php
+++ 16_ArithmeticOperators/practice.php
@@ -1,68 +1,90 @@
 <?php
-	
-	// Constants
 
-	
-	// Custom Variables
+    // Constants
+    define("TITLE", "Arithmetic Operators");
 
+    // Custom Variables
+    $myName = "Sunny";
+    $lessonNum = 16;
 
 ?>
 
 <!DOCTYPE html>
-<html>
-	<head>
-		<title>PHP <!-- TITLE --></title>
-		<link href="../assets/styles.css" rel="stylesheet">
-	</head>
-	<body>
-		<div class="wrapper">
-			<a href="/" title="Back to directory" id="logo">
-				<img src="../assets/img/logo.png" alt="PHP">
-			</a>
-			
-			<h1>Tutorial <!-- LESSON NUMBER -->: <small><!-- TITLE --></small></h1>
-			<hr>
-			
-			<h2>Your Example</h2>
-			
-			<div class="sandbox">
-				
-				<h3>Addition <code>+</code></h3>
-				<?php
-					// your code here
-				?>
-				
-				<h3>Subtraction <code>-</code></h3>
-				<?php
-					// your code here
-				?>
-				
-				<h3>Multiplication <code>*</code></h3>
-				<?php
-					// your code here
-				?>
-				
-				<h3>Division <code>/</code></h3>
-				<?php
-					// your code here
-				?>
-				
-				<h3>Modulus <code>%</code></h3>
-				<?php
-					// your code here
-				?>
-				
-			</div><!-- end sandbox -->
-			
-			<a href="index.php" class="button">Back to the lecture</a>
-			
-			<hr>
-			
-			<small>&copy;<!-- YEAR --> - <!-- NAME --></small>
-		</div><!-- end wrapper -->
-		
-		<div class="copyright-info">
-			<?php include('../assets/includes/copyright.php'); ?>
-		</div><!-- end copyright-info -->
-	</body>
+<html lang="en">
+<head>
+    <title>PHP <?php echo TITLE; ?></title>
+    <link href="../assets/styles.css" rel="stylesheet">
+</head>
+<body>
+<div class="wrapper">
+    <a href="/" title="Back to directory" id="logo">
+        <img src="../assets/img/logo.png" alt="PHP">
+    </a>
+
+    <h1>Tutorial <?php echo $lessonNum; ?>: <small><?php echo TITLE; ?></small></h1>
+    <hr>
+
+    <h2>Final Example</h2>
+    <p>Make sure to view the source code in your editor!</p>
+
+    <div class="sandbox">
+
+        <h3>Addition <code>+</code></h3>
+        <?php
+            $a = 7;
+            $b = 19;
+            $c = ($a + $b);
+
+            echo "$a + $b is equal to $c";
+        ?>
+
+        <h3>Subtraction <code>-</code></h3>
+        <?php
+            $a = 7;
+            $b = 19;
+            $c = ($a - $b);
+
+            echo "$a - $b is equal to $c";
+        ?>
+
+        <h3>Multiplication <code>*</code></h3>
+        <?php
+            $a = 7;
+            $b = 19;
+            $c = ($a * $b);
+
+            echo "$a &times; $b is equal to $c";
+        ?>
+
+        <h3>Division <code>/</code></h3>
+        <?php
+            $a = 7;
+            $b = 19;
+            $c = ($a / $b);
+
+            echo "$a / $b is equal to $c";
+        ?>
+
+        <h3>Modulus <code>%</code></h3>
+        <?php
+            $a = 7;
+            $b = 19;
+            $c = ($a % $b);
+
+            echo "$a % $b is equal to $c";
+        ?>
+
+    </div><!-- end sandbox -->
+
+    <a href="practice.php" class="button">Check out your example</a>
+
+    <hr>
+
+    <small>&copy;<?php echo date('Y'); ?> - <?php echo $myName; ?></small>
+</div><!-- end wrapper -->
+
+<div class="copyright-info">
+    <?php include('../assets/includes/copyright.php'); ?>
+</div><!-- end copyright-info -->
+</body>
 </html>
