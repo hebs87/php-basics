646b72ec1cc386c41efbfe973981972d4a0256a7
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-05-23 13:36:22 +0100

    Added array with member information and dynamically displayed in team.php template

--- 26_Final/Student/includes/arrays.php
+++ 26_Final/Student/includes/arrays.php
@@ -20,4 +20,33 @@
         ),
     );
 
+    // Team Members
+    $teamMembers = array(
+        array(
+            'name' => 'Frankie III',
+            'position' => 'Owner',
+            'bio' => 'Frankie is the great grandson of the original Franklin. He is the owner of Franklin\s Fine Dining.
+             He cooks a mean Fritatta!',
+            // Only reference the name of the image, the path will be set in the HTML file
+            'img' => 'frankie'
+        ),
+        array(
+            'name' => 'Francis',
+            'position' => 'General Manager',
+            'bio' => 'Francis knows her stuff. The big siste of Frankie himself, she runs the show. Don\t miss her
+             Margarita Mondays!',
+            // Only reference the name of the image, the path will be set in the HTML file
+            'img' => 'francis'
+        ),
+        array(
+            'name' => 'Carlos',
+            'position' => 'Head Chef',
+            'bio' => 'Carlos is the epitome of the phrase &ldquo;Don\'t judge a book by it\'s cover&rdquo; &mdash;
+             You simply cannot find a better chef.',
+            // Only reference the name of the image, the path will be set in the HTML file
+            'img' => 'carlos'
+        ),
+    );
+
 ?>
+
